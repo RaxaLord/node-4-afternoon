d4f99085c0e1837199d34b4dad595e8995de3c54
Raxa Hansana <raxah@Raxas-MacBook-Pro.local>
2020-04-07 19:58:49 -0700

    completed - passed all tests

--- server/index.js
+++ server/index.js
@@ -3,6 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const checkForSession = require('./middlewares/checkForSession');
 const swagController = require('./controllers/swagController');
+const {
+  login,
+  register,
+  signout,
+  getUser,
+} = require('./controllers/authController');
+
+const cartController = require('./controllers/cartController');
+const { search } = require('./controllers/searchController');
 
 const app = express();
 
@@ -17,8 +26,17 @@ app.use(
   }),
 );
 app.use(checkForSession);
+app.use(express.static(`${__dirname}/../build`));
 
 app.get('/api/swag', swagController.read);
+app.post('/api/login', login);
+app.post('/api/register', register);
+app.post('/api/signout', signout);
+app.get('/api/user', getUser);
+app.post('/api/cart/checkout', cartController.checkout);
+app.post('/api/cart/:id', cartController.add);
+app.delete('/api/cart/:id', cartController.delete);
+app.get('/api/search', search);
 
 app.listen(
   SERVER_PORT,
